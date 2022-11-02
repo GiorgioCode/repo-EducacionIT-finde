3a115636301cb41d931b96ddfe8a2b662d31949b
Jorge Angel PAEZ <83848476+GiorgioCode@users.noreply.github.com>
2022-11-02 10:10:51 -0300

    modificacion práctica

--- CLASE_01/main.js
+++ CLASE_01/main.js
@@ -64,7 +64,67 @@ numero_muestra++ //numero_muestra = 36
 numero_muestra2-- //numero_muestra = 24
 //es lo mismo que escribir numero_muestra = numero_muestra - 1
 
+//  OPERADORES INCREMENTACION Y DECREMENTACION  //
+var numero_muestra = 35
+var numero_muestra2 = 25
 
+//OPERADORES DE COMPARACION
+var cuarenta = 40
+var treinta = 30
+//MAYOR QUE
+var mayorque = 0
+mayorque = cuarenta > treinta //mayorque = true
+mayorque = treinta > cuarenta //mayorque = false
+//MENOR QUE
+menorque = 0
+menorque = cuarenta < treinta //menorque = false
+menorque = treinta < cuarenta //menorque = true
+//MAYOR O IGUAL QUE
+mayorigual = 0
+mayorigual = cuarenta >= treinta //mayorigual = true
+mayorigual = treinta >= cuarenta //mayorigual = false
+mayorigual = treinta >= treinta //mayorigual = true
+
+//MENOR O IGUAL QUE
+var menorigual = 0
+menorigual = cuarenta <= treinta //menorigual = false
+menorigual = treinta <= cuarenta //menorigual = true
+menorigual = treinta <= treinta //menorigual = true
+
+//IGUAL QUE
+var igualque = 0
+igualque = cuarenta == cuarenta //igualque = true
+igualque = cuarenta == treinta //igualque = false
+
+//DISTINTO que
+var distintoque = 0
+distintoque = cuarenta != cuarenta // distintoque = false
+distintoque = cuarenta != treinta // distintoque = true
+
+console.log("############################################### ")
+console.log("#################  OPERADORES LÓGICOS  ######## ")
+console.log("############################################### ")
+
+//ASIGNAMOS VARIABLES X E Y
+var x = 7;
+var y = 4;
+
+//OPERADOR AND &&
+
+(x < 10 && y > 1); // true 
+(x < 10 && y < 1); // false
+
+//OPERADOR AND ||
+
+(x == 5 || y == 5); // false 
+(x == 7 || y == 0); // true
+(x == 0 || y == 4); // true
+(x == 7 || y == 4); // true
+
+//OPERADOR NEGACIÓN
+
+var verdadero = true
+!verdadero //false
 
 console.log("############################################### ")
 console.log("#################  ENTRADA DATOS  ############# ")
