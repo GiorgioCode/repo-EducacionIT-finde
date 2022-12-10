475ee2ffdac962c88bfe902a8a271f2d68a0f30a
Jorge Angel PAEZ <83848476+GiorgioCode@users.noreply.github.com>
2022-12-09 21:15:44 -0300

    actualizacion clase 5

--- CLASE_02/main.js
+++ CLASE_02/main.js
@@ -1,3 +1,43 @@
+
+// EJEMPLO CON IF 
+var edad = parseInt(prompt("por favor, digite su edad"))
+if (edad >= 0 && edad < 18) {
+    alert("disculpe, solo se permite el ingreso a mayores de 18 años")
+} else {
+    alert("que disfrutes navegando por nuestro sitio!")
+}
+
+// EL PROBLEMA DEL ELSE
+
+alert("le informaremos cuanto le falta contar para llegar a 10")
+var numero = prompt("ingrese un numero menor a 10")
+if ( numero <= 10) {
+    var calculo = 10 - numero
+    alert("le falta contar "+calculo+"numeros mas para llegar a 10")
+} else {
+    alert("debe elegir un numero menor a 10 para poder asistirlo")
+} // en este caso, si se introducen numeros negativos que cumplirian la condicion, rompen el programa
+
+// EJEMPLO CON SWITCH
+var entrada = prompt("Ingresar un nombre");
+//Repetimos hasta que se ingresa "ESC"
+while(entrada != "ESC" ){
+   switch (entrada) {
+       case "ANA":
+            alert("HOLA ANA");
+            break;
+        case "JUAN":
+            alert("HOLA JUAN");
+            break;
+       default:
+           alert("¿QUIÉN SOS?")
+           break;
+   }
+   entrada = prompt("Ingresar un nombre");
+}
+
+
+
 //FOR
 
 // Solicitamos un valor al usuario
@@ -57,52 +97,4 @@ for (let i = 1; i <= ciclos; i++) {
         mostrar = i;
     }
     document.getElementById("fizzbuzz").innerHTML += mostrar+", "
-}
-
-
-// EJEMPLO CON IF 
-var edad = parseInt(prompt("por favor, digite su edad"))
-if (edad >= 0 && edad < 18) {
-    alert("disculpe, solo se permite el ingreso a mayores de 18 años")
-} else {
-    alert("que disfrutes navegando por nuestro sitio!")
-}
-
-// EL PROBLEMA DEL ELSE
-
-alert("le informaremos cuanto le falta contar para llegar a 10")
-var numero = prompt("ingrese un numero menor a 10")
-if ( numero <= 10) {
-    var calculo = 10 - numero
-    alert("le falta contar "+calculo+"numeros mas para llegar a 10")
-} else {
-    alert("debe elegir un numero menor a 10 para poder asistirlo")
-} // en este caso, si se introducen numeros negativos que cumplirian la condicion, rompen el programa
-
-// EJEMPLO CON SWITCH
-var mensaje2 = document.getElementById("mensaje2")
-switch (dia) {
-    case 0:
-        mensaje2.innerText = "que buen domingo para hacer un asado"
-        break;
-    case 1:
-        mensaje2.innerText = "Hoy es LUNES! hoy tenemos clase de JavaScript!"
-        break;
-    case 2:
-        mensaje2.innerText = "que buen MARTES para estudiar JavaScript"
-        break;
-    case 3:
-        mensaje2.innerText = "Hoy es MIERCOLES! hoy tenemos clase de JavaScript!"
-        break;
-    case 4:
-        mensaje2.innerText = "que buen JUEVES para estudiar JavaScript"
-        break;
-    case 5:
-        mensaje2.innerText = "que buen VIERNES finalizar la semana"
-        break;
-    case 6:
-        mensaje2.innerText = "Hoy es SABADO! estamos en fin de semana"
-        break;
-    default:
-        break;
 }
